8dadc3b0126e5450dda5a2b31fcb74005265170a
Jeremy Kithome <jeremy.kithome@andela.com>
2016-12-08 19:51:15 +0300

    Add dotenv config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./server/routes');
+require('dotenv').config();
 
 app.use(cors());
 routes(app);
